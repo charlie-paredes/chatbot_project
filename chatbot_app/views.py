efd6fc9cd45ee39c6ffc446b41d7c8997883769f
charlie <you@example.com>
2024-06-11 17:34:53 -0700

    error handling

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 import google.generativeai as genai
+from google.generativeai.types.generation_types import StopCandidateException
 
 def chatbot(request):
     genai.configure(api_key='AIzaSyCKjBW9EB9XSQNwh__rARG6rc4nu8tTr6w')
@@ -17,15 +18,15 @@ def chatbot(request):
         media_choice = request.POST.get('media_choice', '')
 
         if user_in and character_choice and media_choice:
-            response = chat.send_message(f"respond to the following as if you were {character_choice} from {media_choice}: {user_in}")
-           
-            if response.parts:  # Check if the response contains a valid Part
-                print("Bot:", response.text)
-                return JsonResponse({'character_choice':character_choice, 'media':media_choice, 'user_inp': user_in, 'generated_text': response.text})
+            response = None
+            try:
+                response = chat.send_message(f"respond to the following as if you were {character_choice} from {media_choice}: {user_in}")
+            except StopCandidateException:
+                response = type('', (), {})()
+                response.text = "Sorry, I can't assist with that."
+            print("Bot:", response.text)
+            return JsonResponse({'character_choice':character_choice, 'media':media_choice, 'user_inp': user_in, 'generated_text': response.text})
                 
-            else:
-                response.text = "Sorry, I couldn't generate a response."
-                return JsonResponse({'character_choice':character_choice, 'media':media_choice, 'user_inp': user_in, 'generated_text': response.text})
         else:
             return HttpResponse("Please provide user_in, character_choice, and media_choice as GET parameters.")
     else:
